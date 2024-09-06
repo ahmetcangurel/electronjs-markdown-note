3e5a3c2c0e5246bcf0c57ef6d79e4fc9bf2f45fb
cangurelfuturecode <cangureliletisim@gmail.com>
2024-09-06 17:43:26 +0300

    removed flex class from content layout

--- src/renderer/src/components/AppLayout.tsx
+++ src/renderer/src/components/AppLayout.tsx
@@ -23,7 +23,7 @@ export const Sidebar = ({ className, children, ...props }: ComponentProps<'aside
 export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
   ({ children, className, ...props }, ref) => {
     return (
-      <div ref={ref} {...props} className={twMerge('flex flex-1 overflow-auto', className)}>
+      <div ref={ref} {...props} className={twMerge('flex-1 overflow-auto', className)}>
         {children}
       </div>
     )
