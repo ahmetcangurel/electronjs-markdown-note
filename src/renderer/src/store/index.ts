ab843ccf62832292053cfdddf65ade9875a17469
ahmetcangurel <91556629+ahmetcangurel@users.noreply.github.com>
2024-09-08 15:10:47 +0300

    Fixed note title store

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -9,7 +9,8 @@ export const selectedNoteIndexAtom = atom<number | null>(null)
 export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
-  if (!selectedNoteIndex) return null
+
+  if (selectedNoteIndex == null) return null
 
   const selectedNote = notes[selectedNoteIndex]
 
@@ -18,4 +19,3 @@ export const selectedNoteAtom = atom((get) => {
     content: ` Hello from Notes ${selectedNoteIndex}!`
   }
 })
-
